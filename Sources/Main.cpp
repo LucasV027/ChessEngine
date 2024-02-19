022c644a3dfeaec615e55d1bea51212a38bf2861
p2008378 <lucas.violent@etu.univ-lyon1.fr>
2024-02-19 16:45:32 +0100

    Gitignore, CMake & code skeleton

---
+++ Sources/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+
+int main(void)
+{
+    std::cout << "Hello world !" << std::endl;
+}
